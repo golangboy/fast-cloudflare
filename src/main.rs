c1fce047ded7e269c7d89869059520b6fce2d6c3
golangboy <golangboy@qq.com>
2024-02-07 20:07:28 +0800

    重命名

--- src/main.rs
+++ src/main.rs
@@ -8,40 +8,37 @@ mod utils;
 use rand::random;
 use std::time::Duration;
 
-async fn first_stage() -> Result<(), Box<dyn std::error::Error>> {
-    let cf_ipv4 = utils::get_all_ipv4().await?;
-    let mut v = Vec::new();
-    let result_fs = tokio::fs::File::create("result/live_ip.txt").await?;
-    let tcp_result_fslock: Arc<tokio::sync::Mutex<tokio::fs::File>> =
-        Arc::new(tokio::sync::Mutex::new(result_fs));
-    let pb = ProgressBar::new(cf_ipv4.len() as u64);
-    let pb_t = Arc::new(tokio::sync::Mutex::new(pb));
+async fn perform_first_stage() -> Result<(), Box<dyn std::error::Error>> {
+    let cloudflare_ipv4_addresses = utils::get_all_ipv4().await?;
+    let mut tasks = Vec::new();
+    let live_ip_file = tokio::fs::File::create("result/live_ip.txt").await?;
+    let live_ip_file_lock: Arc<tokio::sync::Mutex<tokio::fs::File>> =
+        Arc::new(tokio::sync::Mutex::new(live_ip_file));
+    let progress_bar = ProgressBar::new(cloudflare_ipv4_addresses.len() as u64);
+    let progress_bar_lock = Arc::new(tokio::sync::Mutex::new(progress_bar));
 
-    let ipv4_int = cf_ipv4.into_iter();
-    let ipv4_int_t = Arc::new(tokio::sync::Mutex::new(ipv4_int));
-    for _i in 1..500 {
-        let _temp = ipv4_int_t.clone();
-        let _result_fslock = tcp_result_fslock.clone();
-        let _pb_t = pb_t.clone();
-        let a = tokio::spawn(async move {
-            let __temp = _temp.clone();
-            let __result_fslock = _result_fslock.clone();
-            let __pb_t = _pb_t.clone();
+    let ipv4_addresses_iter = cloudflare_ipv4_addresses.into_iter();
+    let ipv4_addresses_iter_lock = Arc::new(tokio::sync::Mutex::new(ipv4_addresses_iter));
+    for _ in 1..500 {
+        let addresses_iter_clone = ipv4_addresses_iter_lock.clone();
+        let live_ip_file_clone = live_ip_file_lock.clone();
+        let progress_bar_clone = progress_bar_lock.clone();
+        let task = tokio::spawn(async move {
             loop {
-                let ipv4_t = __temp.lock().await.next();
-                __pb_t.lock().await.inc(1);
-                match ipv4_t {
-                    Some(mut ipv4_addr) => {
-                        let _temp = tokio::net::TcpStream::connect(ipv4_addr.clone() + ":80");
-                        let _temp2 =
-                            tokio::time::timeout(tokio::time::Duration::new(1, 0), _temp).await;
-                        match _temp2 {
-                            Ok(v) => match v {
-                                Ok(mut _vv) => {
-                                    let mut _temp3 = __result_fslock.lock().await;
-                                    ipv4_addr.push('\n');
-                                    _temp3.write(ipv4_addr.as_bytes()).await.unwrap();
-                                    _ = _vv.shutdown().await;
+                let next_ipv4_address = addresses_iter_clone.lock().await.next();
+                progress_bar_clone.lock().await.inc(1);
+                match next_ipv4_address {
+                    Some(mut ipv4_address) => {
+                        let connection_attempt = tokio::net::TcpStream::connect(ipv4_address.clone() + ":80");
+                        let connection_result =
+                            tokio::time::timeout(tokio::time::Duration::new(1, 0), connection_attempt).await;
+                        match connection_result {
+                            Ok(result) => match result {
+                                Ok(mut connection) => {
+                                    let mut file_handle = live_ip_file_clone.lock().await;
+                                    ipv4_address.push('\n');
+                                    file_handle.write(ipv4_address.as_bytes()).await.unwrap();
+                                    _ = connection.shutdown().await;
                                 }
                                 Err(_) => {}
                             },
@@ -54,58 +51,56 @@ async fn first_stage() -> Result<(), Box<dyn std::error::Error>> {
                 }
             }
         });
-        v.push(a);
+        tasks.push(task);
     }
-    join_all(v).await;
-    tcp_result_fslock.lock().await.sync_all().await?;
+    join_all(tasks).await;
+    live_ip_file_lock.lock().await.sync_all().await?;
     return Ok(());
 }
 
-async fn second_stage() -> Result<(), Box<dyn std::error::Error>> {
-    let ping_result = std::fs::File::create("result/ping_ip.txt").unwrap();
-    let ping_result_lock = Arc::new(tokio::sync::Mutex::new(ping_result));
-    let tcp_result = std::fs::read_to_string("result/live_ip.txt").unwrap();
-    let ipv4s: Vec<String> = tcp_result
+async fn perform_second_stage() -> Result<(), Box<dyn std::error::Error>> {
+    let ping_result_file = std::fs::File::create("result/ping_ip.txt").unwrap();
+    let ping_result_file_lock = Arc::new(tokio::sync::Mutex::new(ping_result_file));
+    let live_ip_file_content = std::fs::read_to_string("result/live_ip.txt").unwrap();
+    let live_ipv4_addresses: Vec<String> = live_ip_file_content
         .split("\n")
         .filter(|&s| !s.is_empty())
         .map(|s| s.to_owned())
         .collect();
-    let pb = ProgressBar::new(ipv4s.len() as u64);
-    let mut v = Vec::new();
-    let ipv4_t = Arc::new(tokio::sync::Mutex::new(ipv4s.into_iter()));
-    for _i in 1..10 {
-        let _ipv4 = ipv4_t.clone();
-        let _ping_result_lock = ping_result_lock.clone();
-        let _pb = pb.clone();
-        let a = tokio::spawn(async move {
+    let progress_bar = ProgressBar::new(live_ipv4_addresses.len() as u64);
+    let mut tasks = Vec::new();
+    let live_ipv4_addresses_iter_lock = Arc::new(tokio::sync::Mutex::new(live_ipv4_addresses.into_iter()));
+    for _ in 1..10 {
+        let addresses_iter_clone = live_ipv4_addresses_iter_lock.clone();
+        let ping_result_file_clone = ping_result_file_lock.clone();
+        let progress_bar_clone = progress_bar.clone();
+        let task = tokio::spawn(async move {
             loop {
-                let ipv4 = _ipv4.lock().await.next();
-                let __ping_result_lock = _ping_result_lock.clone();
-                let __pb = _pb.clone();
-                __pb.inc(1);
-                match ipv4 {
-                    Some(ipv4) => {
+                let next_ipv4_address = addresses_iter_clone.lock().await.next();
+                progress_bar_clone.inc(1);
+                match next_ipv4_address {
+                    Some(ipv4_address) => {
                         let client_v4 = Client::new(&Config::default()).unwrap();
                         let mut pinger = client_v4
-                            .pinger(ipv4.parse().unwrap(), PingIdentifier(random()))
+                            .pinger(ipv4_address.parse().unwrap(), PingIdentifier(random()))
                             .await;
                         pinger.timeout(Duration::from_secs(1));
                         let payload = &[1u8];
                         match pinger.ping(PingSequence::from(1), payload).await {
-                            Ok((IcmpPacket::V4(_packet), dur)) => {
-                                let mut ipv4_t = String::from(ipv4);
-                                ipv4_t.push(' ');
-                                ipv4_t.push_str(&dur.as_millis().to_string());
-                                ipv4_t.push('\n');
-                                let mut t = __ping_result_lock.lock().await;
-                                t.write(ipv4_t.as_bytes()).unwrap();
+                            Ok((IcmpPacket::V4(_packet), duration)) => {
+                                let mut result_line = String::from(ipv4_address);
+                                result_line.push(' ');
+                                result_line.push_str(&duration.as_millis().to_string());
+                                result_line.push('\n');
+                                let mut file_handle = ping_result_file_clone.lock().await;
+                                file_handle.write(result_line.as_bytes()).unwrap();
                             }
-                            Ok((IcmpPacket::V6(_packet), _dur)) => {}
+                            Ok((IcmpPacket::V6(_packet), _duration)) => {}
                             Err(_) => {
-                                let mut ipv4_t = String::from(ipv4);
-                                ipv4_t.push_str(" timeout\n");
-                                let mut t = __ping_result_lock.lock().await;
-                                t.write(ipv4_t.as_bytes()).unwrap();
+                                let mut result_line = String::from(ipv4_address);
+                                result_line.push_str(" timeout\n");
+                                let mut file_handle = ping_result_file_clone.lock().await;
+                                file_handle.write(result_line.as_bytes()).unwrap();
                             }
                         }
                     }
@@ -115,19 +110,19 @@ async fn second_stage() -> Result<(), Box<dyn std::error::Error>> {
                 }
             }
         });
-        v.push(a);
+        tasks.push(task);
     }
-    join_all(v).await;
-    ping_result_lock.lock().await.sync_all().unwrap();
+    join_all(tasks).await;
+    ping_result_file_lock.lock().await.sync_all().unwrap();
     return Ok(());
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     _ = std::fs::create_dir("result");
-    println!("first stage");
-    first_stage().await?;
-    println!("second stage");
-    second_stage().await?;
+    println!("Performing first stage");
+    perform_first_stage().await?;
+    println!("Performing second stage");
+    perform_second_stage().await?;
     return Ok(());
 }
